731da8c69fbd179afe9c656d5cbd7a5a13e1c36e
jfeng986 <jfeng986@gmail.com>
2023-09-05 00:27:21 +0800

    dev

--- apps/api/internal/svc/servicecontext.go
+++ apps/api/internal/svc/servicecontext.go
@@ -2,22 +2,22 @@ package svc
 
 import (
 	"snkrs/apps/api/internal/config"
-	"snkrs/apps/order/rpc/orderclient"
-	"snkrs/apps/product/rpc/productclient"
+	"snkrs/apps/order/rpc/order"
+	"snkrs/apps/product/rpc/product"
 
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
 	Config     config.Config
-	OrderRPC   orderclient.Order
-	ProductRPC productclient.Product
+	OrderRPC   order.Order
+	ProductRPC product.Product
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:     c,
-		OrderRPC:   orderclient.NewOrder(zrpc.MustNewClient(c.OrderRPC)),
-		ProductRPC: productclient.NewProduct(zrpc.MustNewClient(c.ProductRPC)),
+		OrderRPC:   order.NewOrder(zrpc.MustNewClient(c.OrderRPC)),
+		ProductRPC: product.NewProduct(zrpc.MustNewClient(c.ProductRPC)),
 	}
 }
