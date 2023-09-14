3c2427c59ec2967a9799d4d8e57ce9bc5791db59
Junyi Feng <jfeng986@gmail.com>
2023-09-14 14:47:16 +0800

    Update wrk.lua

--- wrk.lua
+++ wrk.lua
@@ -3,7 +3,7 @@ wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
 
 function request()
   local userID = math.random(19, 38)
-  local productID = 2
+  local productID = math.random(1, 16)
   local body = "user_id=" .. userID .. "&product_id=" .. productID
   return wrk.format(nil, nil, nil, body)
 end
